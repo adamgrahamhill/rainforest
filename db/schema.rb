6dd7f7dbd5d539a3aa748f59fbf8335823d69c56
Adam Hill <enjoilif3@gmail.com>
2016-07-16 11:26:49 -0400

    Added img field to form, working on view/show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716005902) do
+ActiveRecord::Schema.define(version: 20160716145119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160716005902) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "url"
   end
 
   create_table "skateboards", force: :cascade do |t|
