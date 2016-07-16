f71a6a12197a2f3e18ac9c0379d01e680b655662
Jake Sena <jakesena1994@gmail.com>
2016-07-16 11:58:05 -0400

    Add price formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716145119) do
+ActiveRecord::Schema.define(version: 20160716153613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,13 @@ ActiveRecord::Schema.define(version: 20160716145119) do
     t.string   "url"
   end
 
-  create_table "skateboards", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price_in_cents"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id", using: :btree
   end
 
+  add_foreign_key "reviews", "products"
 end
